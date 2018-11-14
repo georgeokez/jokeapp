049c0ddbb9f1178a36f9bcdbd3aa600a521c704b
george <gokereka@teamapt.com>
2018-11-14 23:19:40 +0100

    Changed the angular backend to point to live

--- src/main/resources/static/app/controller/jokeController.js
+++ src/main/resources/static/app/controller/jokeController.js
@@ -2,7 +2,7 @@
 
 App.controller('JokeController', ['$scope','$http', function ($scope, $http) {
 
-    var URL = "https://34.227.214.181:80/api/v1/joke";
+    var URL = "https://52.54.218.210:80/api/v1/joke";
 
     $scope.joke = "";
 
